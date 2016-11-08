4822d5cf43408518282d27e13a17a3b2af292eb5
yixiong <yixiong@retail-tek.com>
2016-11-08 14:54:37 +0800

    init some core file

---
+++ component/datepickerDemo/template/datepicker.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+            'other-component':OtherComponent,
+            HeaderComponent,
+        }
+    }
+</script>
+<style>
+
+</style>
