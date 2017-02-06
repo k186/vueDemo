78db3cc90f73d347930cc2a5bc403cbf29d5de8f
yixiong@retail-tek.com <yixiong@retail-tek.com>
2017-02-06 18:34:12 +0800

    play todo detail

--- src/components/MusicPlayer/payer.vue
+++ src/components/MusicPlayer/payer.vue
@@ -1,22 +1,150 @@
 <template>
-   <div class="player-box">
-       <div class="player-box-bg">
-           <div class="player-box-poster">
-               <img src="../../../static/imgs/poster/defalut.png" alt="海报" class="player-box-poster-img">
-           </div>
-           <div class="player-box-text">
-               <div class="player-box-text-title">歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名歌名</div>
-               <div class="player-box-text-lyric">歌词歌词歌词歌词歌词歌词歌词歌词歌词歌词歌词歌词歌词歌词歌词歌词</div>
-           </div>
-           <div class="player-box-btn">
-               <i class="player-box-btn-play icon">&#xe675;</i>
-               <i class="player-box-btn-playList icon">&#xe841;</i>
-           </div>
-       </div>
-   </div>
+    <div class="player-box">
+        <div class="player-box-bg">
+            <transition-group class="player-box-currentPlay" tag="div" :name="swipeChange">
+                <div class="player-box-poster" :key="PlayerComp.currentPlay.poster">
+                    <img v-if="PlayerComp.currentPlay.poster!=''" :src="PlayerComp.currentPlay.poster" alt="海报" class="player-box-poster-img">
+                    <img v-if="PlayerComp.currentPlay.poster==''" src="../../../static/imgs/poster/defalut.png" alt="海报" class="player-box-poster-img">
+                </div>
+                <div class="player-box-text" :key="PlayerComp.currentPlay.poster">
+                    <div v-show="PlayerComp.currentPlay.title==''" class="player-box-text-title">QQ音乐</div>
+                    <div v-show="PlayerComp.currentPlay.title!=''" class="player-box-text-title">{{PlayerComp.currentPlay.title}}</div>
+                    <div class="player-box-text-lyric"></div>
+                </div>
+            </transition-group>
+            <div class="player-box-btn">
+                <i v-if="PlayerComp.playStatus==0" class="player-box-btn-play icon" @click="play">&#xe675;</i>
+                <i v-if="PlayerComp.playStatus==1" class="player-box-btn-play icon" @click="pause">&#xe6ab;</i>
+                <i class="player-box-btn-playList icon">&#xe841;</i>
+            </div>
+        </div>
+        <audio id="audio" :src="PlayerComp.currentPlay.url"></audio>
+    </div>
 </template>
 <script>
+    import {mapActions, mapGetters} from 'vuex'
     export default{
-        name:'player'
+        name: 'player',
+        data(){
+            return {
+                playOrder: [],
+                currentIndex: 0,
+                swipeChange:''
+            }
+        },
+        computed: mapGetters({
+            PlayerComp: 'PlayerComp'
+        }),
+        mounted(){
+            this.PlayerComp.currentPlay.audio = document.getElementById('audio');
+            let PlayerComp = null;
+            this.$store.dispatch('playerInit', {PlayerComp});
+            this.initPlayOrder();
+        },
+        methods: {
+            play(){
+                if (this.PlayerComp.currentPlay.url != '') {
+                    this.PlayerComp.currentPlay.audio.currentTime = this.PlayerComp.currentPlay.currentTime;
+                    this.PlayerComp.currentPlay.audio.play();
+                    let PlayerComp = {
+                        currentPlay: {
+                            audio: this.PlayerComp.currentPlay.audio,
+                            duration: this.PlayerComp.currentPlay.audio.duration,
+                            currentTime: this.PlayerComp.currentPlay.currentTime,
+                        }
+                    };
+                    this.$store.dispatch('playerPlay', {PlayerComp});
+                    this.isPlayEnd('start');
+                }
+            },
+            pause(){
+                if (this.PlayerComp.currentPlay.url != '') {
+                    this.PlayerComp.currentPlay.audio.pause();
+                    let PlayerComp = {
+                        currentPlay: {
+                            audio: this.PlayerComp.currentPlay.audio,
+                            duration: this.PlayerComp.currentPlay.audio.duration,
+                            currentTime: this.PlayerComp.currentPlay.audio.currentTime,
+                        }
+                    };
+                    this.$store.dispatch('playerPause', {PlayerComp});
+                    this.isPlayEnd('pause');
+                }
+            },
+            next(){
+                this.currentIndex += 1;
+                if (this.currentIndex > this.playOrder.length - 1) {
+                    this.currentIndex = 0
+                }
+                this.swipeChange='right2left';
+                this.getUid();
+                //todo when load to play
+            },
+            previous(){
+                this.currentIndex -= 1;
+                if (this.currentIndex < 0) {
+                    this.currentIndex = 0
+                }
+                this.swipeChange='left2right';
+                this.getUid();
+                //todo when load to play
+            },
+            getUid(){
+                let uid = this.playOrder[this.currentIndex];
+                this.$store.dispatch('playerNext', {uid})
+            },
+            initPlayOrder(){
+                let uidArr = [];
+                let DataArr = this.PlayerComp.playList.list;
+                for (let i = 0; i < DataArr.length; i++) {
+                    uidArr[i] = DataArr[i].song.uid;
+                }
+                /*set playOrder list*/
+                if (this.PlayerComp.playType == 1) {
+                    /*random*/
+                    uidArr.sort(function () {
+                        return 0.5 - Math.random()
+                    });
+                    this.playOrder = uidArr;
+                } else if (this.PlayerComp.playType == 2 || this.PlayerComp.playType == 3) {
+                    /*order*/
+                    this.playOrder = uidArr;
+                }
+            },
+            isPlayEnd(type){
+                if (type == 'start') {
+                    let that = this;
+                    let loop = setInterval(function () {
+                        let currentTime = that.PlayerComp.currentPlay.audio.currentTime;
+                        let duration = that.PlayerComp.currentPlay.duration;
+                        let PlayerComp = {
+                            currentPlay: {
+                                process: currentTime / duration
+                            }
+                        };
+                        that.$store.dispatch('playerProcess', {PlayerComp});
+                        /*auto end*/
+                        if (currentTime == duration) {
+                            that.isPlayEnd('pause');
+                            that.pause();
+                            let PlayerComp = {
+                                currentPlay: {
+                                    audio: that.PlayerComp.currentPlay.audio,
+                                    duration: 0,
+                                    currentTime: 0,
+                                }
+                            };
+                            that.$store.dispatch('playerPause', {PlayerComp});
+                            that.next();
+                            clearInterval(loop);
+                        }
+                        /*press pause*/
+                        if (that.PlayerComp.playStatus == 0) {
+                            clearInterval(loop);
+                        }
+                    }, 1000)
+                }
+            }
+        }
     }
 </script>
--- src/vuex/modules/player.js
+++ src/vuex/modules/player.js
@@ -4,16 +4,195 @@
 import * as TYPE from '../mutation-types'
 
 const state = {
-    player: {
-        playerList:[]
+    PlayerComp: {
+        playType: 2, /*1 random order singleLoop*/
+        playStatus: 0, /*0 pause 1 play*/
+        currentPlay: {
+            audio: {},
+            currentTime: 190,
+            process:0,
+            duration: 0,
+            url: '',
+            poster:'',
+            uid:'',
+            title: '',
+            artist: '',
+            album: '',
+        },
+        playList: {
+            list: [
+                {
+                    isPlay: true,
+                    song: {
+                        url: 'http://k186studio.com/static/music/8545.mp3',
+                        poster: '../../../static/imgs/poster/8545.jpg',
+                        title: 'Try Everything (尝试一切)',
+                        artist: 'Shakira',
+                        album: 'Zootopia (Original Motion Picture Soundtrack) (疯狂动物城 电影原声带)',
+                        uid: 8545, /*用这个去匹配下载列表 HASH 来设置isDownload*/
+                        markTitle: {
+                            quality: 1, /*0 normal 1 hq 2 sq */
+                            exclusive: 1, /* 独家 0 1*/
+                            mv: 1/*mv 0 1*/
+                        }
+                    }
+                },
+                {
+                    isPlay: false,
+                    song: {
+                        url: 'http://k186studio.com/static/music/8548.mp3',
+                        poster: '../../../static/imgs/poster/8548.jpg',
+                        title: 'El Mismo Sol (Under The Same Sun) (B-Case Remix)',
+                        artist: 'Alvaro Soler',
+                        album: 'El Mismo Sol (Under The Same Sun) (B-Case Remix)',
+                        uid: 8548, /*用这个去匹配下载列表 HASH 来设置isDownload*/
+                        markTitle: {
+                            quality: 1, /*0 normal 1 hq 2 sq */
+                            exclusive: 1, /* 独家 0 1*/
+                            mv: 1/*mv 0 1*/
+                        }
+                    }
+                },
+                {
+                    isPlay: false,
+                    song: {
+                        url: 'http://k186studio.com/static/music/8546.mp3',
+                        poster: '../../../static/imgs/poster/8546.jpg',
+                        title: 'I Like Chopin',
+                        artist: 'Gazebo',
+                        album: 'Portrait',
+                        uid: 8546, /*用这个去匹配下载列表 HASH 来设置isDownload*/
+                        markTitle: {
+                            quality: 1, /*0 normal 1 hq 2 sq */
+                            exclusive: 1, /* 独家 0 1*/
+                            mv: 1/*mv 0 1*/
+                        }
+                    }
+                },
+                {
+                    isPlay: false,
+                    song: {
+                        url: 'http://k186studio.com/static/music/8543.mp3',
+                        poster: '../../../static/imgs/poster/8543.jpg',
+                        title: 'Heroes',
+                        artist: 'Alesso',
+                        album: 'Heroes (we could be)',
+                        uid: 8543, /*用这个去匹配下载列表 HASH 来设置isDownload*/
+                        markTitle: {
+                            quality: 1, /*0 normal 1 hq 2 sq */
+                            exclusive: 1, /* 独家 0 1*/
+                            mv: 1/*mv 0 1*/
+                        }
+                    }
+                },
+                {
+                    isPlay: false,
+                    song: {
+                        url: 'http://k186studio.com/static/music/1232.mp3',
+                        poster: '../../../static/imgs/poster/1232.jpg',
+                        title: 'Reality',
+                        artist: 'A.M.7',
+                        album: 'Reality (Single Version)',
+                        uid: 1232, /*用这个去匹配下载列表 HASH 来设置isDownload*/
+                        markTitle: {
+                            quality: 1, /*0 normal 1 hq 2 sq */
+                            exclusive: 1, /* 独家 0 1*/
+                            mv: 1/*mv 0 1*/
+                        }
+                    }
+                }
+            ]
+        }
     }
 };
-/*api getters can use all*/
 const getters = {
-    player: state => state.player
+    PlayerComp: state => state.PlayerComp
 };
-const mutations = {};
-const actions = {};
+const mutations = {
+    [TYPE.PLAYER_EVENT_INIT](state, {currentPlay}){
+        if(currentPlay!=null){
+            state.PlayerComp.currentPlay.url=currentPlay.url;
+            state.PlayerComp.currentPlay.title=currentPlay.title;
+            state.PlayerComp.currentPlay.artist=currentPlay.artist;
+            state.PlayerComp.currentPlay.album=currentPlay.album;
+            state.PlayerComp.currentPlay.uid=currentPlay.uid;
+            state.PlayerComp.currentPlay.poster=currentPlay.poster;
+        }else {
+            if(state.PlayerComp.playList.list.length!=0){
+                state.PlayerComp.currentPlay.url=state.PlayerComp.playList.list[0].song.url;
+                state.PlayerComp.currentPlay.title=state.PlayerComp.playList.list[0].song.title;
+                state.PlayerComp.currentPlay.artist=state.PlayerComp.playList.list[0].song.artist;
+                state.PlayerComp.currentPlay.album=state.PlayerComp.playList.list[0].song.album;
+                state.PlayerComp.currentPlay.uid=state.PlayerComp.playList.list[0].song.uid;
+                state.PlayerComp.currentPlay.poster=state.PlayerComp.playList.list[0].song.poster;
+            }
+        }
+    },
+    [TYPE.PLAYER_EVENT_PROCESS](state, {PlayerComp}){
+        state.PlayerComp.currentPlay.process=PlayerComp.currentPlay.process;
+    },
+    [TYPE.PLAYER_EVENT_PLAY](state, {PlayerComp}){
+        state.PlayerComp.playStatus = 1;
+        state.PlayerComp.currentPlay.audio = PlayerComp.currentPlay.audio;
+        state.PlayerComp.currentPlay.duration = PlayerComp.currentPlay.duration;
+        state.PlayerComp.currentPlay.currentTime = PlayerComp.currentPlay.currentTime;
+    },
+    [TYPE.PLAYER_EVENT_PAUSE](state, {PlayerComp}){
+        state.PlayerComp.playStatus = 0;
+        state.PlayerComp.currentPlay.audio = PlayerComp.currentPlay.audio;
+        state.PlayerComp.currentPlay.duration = PlayerComp.currentPlay.duration;
+        state.PlayerComp.currentPlay.currentTime = PlayerComp.currentPlay.currentTime;
+    },
+    [TYPE.PLAYER_EVENT_NEXT](state, {currentPlay}){
+        state.PlayerComp.currentPlay.uid=currentPlay.uid;
+        state.PlayerComp.currentPlay.url=currentPlay.url;
+        state.PlayerComp.currentPlay.poster=currentPlay.poster;
+        state.PlayerComp.currentPlay.title=currentPlay.title;
+        state.PlayerComp.currentPlay.artist=currentPlay.artist;
+        state.PlayerComp.currentPlay.album=currentPlay.album;
+    }
+};
+const actions = {
+    playerInit({commit}, {currentPlay}){
+        commit(TYPE.PLAYER_EVENT_INIT, {currentPlay})
+    },
+    playerPlay({commit}, {PlayerComp}){
+        commit(TYPE.PLAYER_EVENT_PLAY, {PlayerComp})
+    },
+    playerPause({commit}, {PlayerComp}){
+        commit(TYPE.PLAYER_EVENT_PAUSE, {PlayerComp})
+    },
+    playerNext({commit}, {uid}){
+        let currentPlay={
+            currentTime: 0,
+            process:0,
+            duration: 0,
+            url: '',
+            poster:'',
+            uid:'',
+            title: '',
+            artist: '',
+            album: '',
+        };
+        if(state.PlayerComp.playList.list.length!=0){
+            let songList=state.PlayerComp.playList.list;
+            for(let i=0;i<songList.length;i++){
+                if(songList[i].song.uid==uid){
+                    currentPlay.uid=uid;
+                    currentPlay.url=songList[i].song.url;
+                    currentPlay.poster=songList[i].song.poster;
+                    currentPlay.title=songList[i].song.title;
+                    currentPlay.artist=songList[i].song.artist;
+                    currentPlay.album=songList[i].song.album;
+                }
+            }
+        }
+        commit(TYPE.PLAYER_EVENT_NEXT, {currentPlay})
+    },
+    playerProcess({commit}, {PlayerComp}){
+        commit(TYPE.PLAYER_EVENT_PROCESS, {PlayerComp})
+    },
+};
 export default {
     state,
     getters,
