e41fb36f32ba3399eefc5b425bc3ea2c40b9080c
k186 <k1868548@gmail.com>
2017-03-07 13:54:29 +0800

    test git config

--- src/components/footer/footer.vue
+++ src/components/footer/footer.vue
@@ -4,6 +4,7 @@
       <div>蜀ICP备15002031号-1</div>
       <div>Copyright © Yi Xiong 2013 - 2017</div>
     </div>
+
     <iframe src="https://ghbtns.com/github-btn.html?user=k186&repo=vueDemo&type=star&count=true" frameborder="0"></iframe>
   </div>
 </template>
